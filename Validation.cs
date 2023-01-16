66d61d1d0de2937152a4630a642333ba5e9d28f6
Andreea Mirea <alecsia0811@gmail.com>
2023-01-16 18:00:18 +0200

    part1

---
+++ Validation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mirea_Andreea_Proiect
+{
+    class Validation : Behavior<Editor>
+    {
+        protected override void OnAttachedTo(Editor entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+        protected override void OnDetachingFrom(Editor entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            ((Editor)sender).BackgroundColor = string.IsNullOrEmpty(args.NewTextValue) ? Color.FromArgb("EB8C72") : Color.FromArgb("FFFFFF");
+        }
+    }
+}
