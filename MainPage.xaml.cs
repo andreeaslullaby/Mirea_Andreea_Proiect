024662f3805561f255b9cc10316e04a285706042
Andreea Mirea <alecsia0811@gmail.com>
2023-01-19 13:20:59 +0200

    done

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,9 +14,9 @@ public partial class MainPage : ContentPage
 		count++;
 
 		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+			CounterBtn.Text = $"you have {count} things on your mind";
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+			CounterBtn.Text = $"you have {count} things on your mind";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
